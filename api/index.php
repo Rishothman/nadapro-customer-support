cb6d7a606e96df7ef9d37cb31fe173be2cb8fced
Rishothman <rishanrishothman@gmail.com>
2026-08-20 16:59:39 +0530

    Fix PHP runtime for Vercel

--- api/index.php
+++ api/index.php
@@ -1,9 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(
-    \Illuminate\Http\Request::capture()
+    Request::capture()
 );
